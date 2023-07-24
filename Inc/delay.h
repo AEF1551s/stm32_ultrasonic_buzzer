e1c6846b786827e7ae80cc3f4e080306b6834272
Krisjanis Ivbulis <ivbulisk@gmail.com>
2023-07-24 18:29:35 +0300

    Add uS delay TIM5

--- Inc/delay.h
+++ Inc/delay.h
@@ -3,6 +3,7 @@
 
 #include <stm32f410rx.h>
 #include <stm32f4xx.h>
+#include <timer.h>
 
 #define SYSTICK_LOAD_VAL_MS 16000UL
 
@@ -22,4 +23,19 @@ void delay_ms(uint32_t delay)
     }
 }
 
-#endif //DELAY_H
+void delay_us(uint32_t delay) // Uses TIM5
+{
+    tim5_us_init();
+    for (uint32_t i = 0; i < delay; i++)
+    {
+
+        // Wait for UIF, which resets after 1uS
+        while (!READ_BIT(TIM5->SR, TIM_SR_UIF))
+            ;
+
+        // Clear UIF
+        CLEAR_BIT(TIM5->SR, TIM_SR_UIF);
+    }
+}
+
+#endif // DELAY_H
